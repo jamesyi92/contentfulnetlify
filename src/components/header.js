ade072b91c84551f71c53092d53774eadd0d73ef
James Yi <james.yi@sterlingts.com>
2018-07-26 14:10:39 -0700

    .

--- src/components/header.js
+++ src/components/header.js
@@ -4,17 +4,17 @@ import Link from 'gatsby-link'
 const Header = ({ siteTitle }) => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
     <div className="container">
-      <Link className="navbar-brand">SandBox</Link>
+      <Link className="navbar-brand" to="/">SandBox</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon" />
       </button>
       <div className="collapse navbar-collapse" id="navbarNavDropdown">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <Link exact activeClassName="active" className="nav-link">Blog</Link>
+            <Link exact activeClassName="active" to="/" className="nav-link">Blog</Link>
           </li>
           <li className="nav-item">
-            <Link activeClassName="active" className="nav-link" to="/webinars">Webinars</Link>
+            <Link activeClassName="active" className="nav-link" to="webinars">Webinars</Link>
           </li>
         </ul>
       </div>
